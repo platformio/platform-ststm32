e7ee381f84b03a8a7b5bc5a6164a1b96f8f965e9
Ivan Kravets <me@ikravets.com>
2016-10-14 12:18:14 +0300

    Allow using stlink for stm32duino // Resolve #16

--- builder/main.py
+++ builder/main.py
@@ -137,10 +137,11 @@ if env.subst("$UPLOAD_PROTOCOL") == "gdb":
         UPLOADCMD='$UPLOADER $UPLOADERFLAGS'
     )
 
-if "arduino" in env.subst("$PIOFRAMEWORK"):
+elif env.subst("$UPLOAD_PROTOCOL") in ("serial", "dfu") \
+        and "arduino" in env.subst("$PIOFRAMEWORK"):
     _upload_tool = "serial_upload"
     _upload_flags = ["{upload.altID}", "{upload.usbID}"]
-    if "dfu" in env.subst("$UPLOAD_PROTOCOL"):
+    if env.subst("$UPLOAD_PROTOCOL") == "dfu":
         _upload_tool = "maple_upload"
         _usbids = env.BoardConfig().get("build.hwids")
         _upload_flags = [env.BoardConfig().get("upload.boot_version", 2),
