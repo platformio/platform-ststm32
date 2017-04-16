027a8b6647f793cdbfa9a8ec07a4e9a4e85a2ddf
stevstrong <stevestrong@online.de>
2017-04-16 12:21:43 +0200

    added support for F4 series
    - added board for generic STM32F407VET6
    - changed scripts to handle conrrectly both F1 and F4 families

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -35,49 +35,79 @@ if board.id == "bluepill_f103c8":
     env['LDSCRIPT_PATH'] = board.get("build.ldscript")
     env.ProcessFlags(board.get("build.extra_flags"))
 
+# check Cortex variant to distinguish between F1 and F4 folders
+if "m3" in board.get("build.cpu"):
+	FRAMEWORK_DIR = join(platform.get_package_dir("framework-arduinoststm32"), "STM32F1")
+else:
+	FRAMEWORK_DIR = join(platform.get_package_dir("framework-arduinoststm32"), "STM32F4")
 
-FRAMEWORK_DIR = join(platform.get_package_dir(
-    "framework-arduinoststm32"), "STM32F1")
 FRAMEWORK_VERSION = platform.get_package_version("framework-arduinoststm32")
 assert isdir(FRAMEWORK_DIR)
 
+#common build options for F1 and F4
 env.Append(
-    CCFLAGS=[
-        "--param", "max-inline-insns-single=500",
-        "-march=armv7-m"
-    ],
+	CCFLAGS=[
+		"--param", "max-inline-insns-single=500",
+		"-MMD", "-fno-threadsafe-statics"
+	],
+	
+	CPPDEFINES=[
+		("ARDUINO", 10612),
+		"BOARD_%s" % board.get("build.variant"),
+		("DEBUG_LEVEL", "DEBUG_NONE")
+	],
+	
+	CPPPATH=[join(FRAMEWORK_DIR, "cores", board.get("build.core"))],
 
-    CPPDEFINES=[
-        ("ARDUINO", 10610),
-        "BOARD_%s" % board.get("build.variant"),
-        ("ERROR_LED_PORT", "GPIOB"),
-        ("ERROR_LED_PIN", 1),
-        ("DEBUG_LEVEL", "DEBUG_NONE"),
-        "__STM32F1__",
-        "ARDUINO_ARCH_STM32F1"
-    ],
+	LIBPATH=[
+		join(FRAMEWORK_DIR, "variants",
+			 board.get("build.variant"), "ld")
+	],
 
-    CPPPATH=[
-        join(FRAMEWORK_DIR, "cores", board.get("build.core")),
-        join(FRAMEWORK_DIR, "system", "libmaple"),
-        join(FRAMEWORK_DIR, "system", "libmaple", "include"),
-        join(FRAMEWORK_DIR, "system", "libmaple", "usb", "stm32f1"),
-        join(FRAMEWORK_DIR, "system", "libmaple", "usb", "usb_lib")
-    ],
-
-    LIBPATH=[
-        join(FRAMEWORK_DIR, "variants",
-             board.get("build.variant"), "ld")
-    ],
-
-    LIBS=["c"]
+	LIBS=["c"]
 )
+
+# specific build options
+if "m3" in board.get("build.cpu"):
+	env.Append(
+		CCFLAGS=["-march=armv7-m"],
+
+		CPPDEFINES=[
+			"__STM32F1__",
+			"ARDUINO_ARCH_STM32F1"
+		],
+
+		CPPPATH=[
+			join(FRAMEWORK_DIR, "system\libmaple"),
+			join(FRAMEWORK_DIR, "system\libmaple\include"),
+			join(FRAMEWORK_DIR, "system\libmaple\usb\stm32f1"),
+			join(FRAMEWORK_DIR, "system\libmaple\usb\usb_lib")
+		]
+	)
+else:
+	env.Append(
+		CPPDEFINES=[
+			"__STM32F4__",
+			"ARDUINO_ARCH_STM32F4",
+			"VECT_TAB_BASE"
+		],
+
+		CPPPATH=[
+			join(FRAMEWORK_DIR, "cores", board.get("build.core"), "libmaple"),
+			join(FRAMEWORK_DIR, "cores", board.get("build.core"), "libmaple\usbF4"),
+			join(FRAMEWORK_DIR, "cores", board.get("build.core"), "libmaple\usbF4\VCP"),
+			join(FRAMEWORK_DIR, "cores", board.get("build.core"), "libmaple\usbF4\STM32_USB_OTG_Driver\inc"),
+			join(FRAMEWORK_DIR, "cores", board.get("build.core"), "libmaple\usbF4\STM32_USB_Device_Library\Core\inc"),
+			join(FRAMEWORK_DIR, "cores", board.get("build.core"), "libmaple\usbF4\STM32_USB_Device_Library\Class\cdc\inc")
+		]
+	)
+
 
 for item in ("-nostartfiles", "-nostdlib"):
     if item in env['LINKFLAGS']:
         env['LINKFLAGS'].remove(item)
 
-
+# upload specific options
 if env.subst("$UPLOAD_PROTOCOL") == "dfu":
     if board.id in ("maple", "maple_mini_origin"):
         env.Append(CPPDEFINES=[("VECT_TAB_ADDR", 0x8005000), "SERIAL_USB"])
@@ -92,6 +122,15 @@ if env.subst("$UPLOAD_PROTOCOL") == "dfu":
 else:
     env.Append(CPPDEFINES=[("VECT_TAB_ADDR", 0x8000000)])
 
+# error LED handling
+if "error_led_port" in board.get("build") and "error_led_pin" in board.get("build"):
+	env.Append(CPPDEFINES=[
+		("ERROR_LED_PORT", board.get("build.error_led_port")),
+		("ERROR_LED_PIN",  board.get("build.error_led_pin"))])
+else:
+	env.Append(CPPDEFINES=[
+		("ERROR_LED_PORT", "GPIOB"),
+		("ERROR_LED_PIN", "1")])
 
 if "__debug" in COMMAND_LINE_TARGETS:
     env.Append(CPPDEFINES=[
