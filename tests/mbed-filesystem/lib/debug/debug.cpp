fddbc4b4ddda4fda7dc19c459d1b3dff6879076a
Valerii Koval <valeros@bigmir.net>
2018-06-02 23:01:24 +0300

    Switch to fixed mbed build script // Issue #110

---
+++ tests/mbed-filesystem/lib/debug/debug.cpp
@@ -0,0 +1,4 @@
+#include <mbed.h>
+#include "Debug.h"
+
+Serial debugSerial(PA_2, PA_3, 57600);
---
+++ tests/mbed-filesystem/src/main.cpp
@@ -0,0 +1,11 @@
+#include <mbed.h>
+#include <filesystem/mbed_filesystem.h>
+#include <Debug.h>
+
+int main()
+{
+  for(;;)
+  {
+
+  }
+}
