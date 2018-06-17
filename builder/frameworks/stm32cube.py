b3ec774823fd3416041fa26e0fe6da35ad9a428b
Jokūbas M <ubis@users.noreply.github.com>
2018-06-17 11:38:14 +0300

    Feature/add stmf1 boards (#119)
    * Add Maple RET6 board
    
    * Add Microduino board
    
    * Add boot version for Microduino board
    
    * Add generic STM32F103T* boards
    
    * Add generic STM32F103V B/D boards
    
    * Add generic STM32F103Z C/D/E support
    
    * Remove unnecessary comparison
    
    * Use correct extra flags from STM32Cube
    
    * Revert unnecessary flag replacing

--- builder/frameworks/stm32cube.py
+++ builder/frameworks/stm32cube.py
@@ -43,8 +43,12 @@ STARTUP_FILE_EXCEPTIONS = {
     "stm32f103c8": "startup_stm32f103xb.s",
     "stm32f103r8": "startup_stm32f103xb.s",
     "stm32f103rc": "startup_stm32f103xb.s",
+    "stm32f103t8": "startup_stm32f103xb.s",
     "stm32f103vc": "startup_stm32f103xe.s",
+    "stm32f103vd": "startup_stm32f103xe.s",
     "stm32f103ve": "startup_stm32f103xe.s",
+    "stm32f103zc": "startup_stm32f103xe.s",
+    "stm32f103zd": "startup_stm32f103xe.s",
     "stm32f407ve": "startup_stm32f407xx.s"
 }
 
