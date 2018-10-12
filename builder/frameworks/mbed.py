05d409f530233d483ea2c51301f939f6b0321c6f
Ivan Kravets <me@ikravets.com>
2018-10-13 00:14:29 +0300

    Add info about mbed build repo

--- builder/frameworks/mbed.py
+++ builder/frameworks/mbed.py
@@ -31,6 +31,7 @@ from SCons.Script import Import, SConscript
 
 Import("env")
 
+# https://github.com/platformio/builder-framework-mbed.git
 SConscript(
     join(env.PioPlatform().get_package_dir("framework-mbed"), "platformio",
          "platformio-build.py"))
