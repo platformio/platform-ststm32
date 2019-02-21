19581ce9f4453a0be84a1af6f4cf1d441e23a590
valeros <valeros@bigmir.net>
2019-02-21 21:39:49 +0200

    Print understandable error message when mcu family is not supported

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -22,7 +22,8 @@ kinds of creative coding, interactive objects, spaces or physical experiences.
 http://www.stm32duino.com
 """
 
-from os.path import join
+import sys
+from os.path import join, isfile
 
 from SCons.Script import DefaultEnvironment, SConscript
 
@@ -32,10 +33,16 @@ core = env.BoardConfig().get("build.core", "")
 
 
 if core == "maple":
-    SConscript(
-        join(env.PioPlatform().get_package_dir(
-            "framework-arduinoststm32-maple"),
-            "tools", "platformio-build-%s.py" % mcu[0:7]))
+    build_script = join(
+        env.PioPlatform().get_package_dir("framework-arduinoststm32-maple"),
+        "tools", "platformio-build-%s.py" % mcu[0:7])
+    if isfile(build_script):
+        SConscript(build_script)
+    else:
+        sys.stderr.write(
+            "Error: %s family is not supported by maple core\n" % mcu[0:7])
+        env.Exit(1)
+
 else:
     SConscript(
         join(env.PioPlatform().get_package_dir(
