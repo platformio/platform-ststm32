c3f87946ce195a76d7bb6f95c41701695025c69c
valeros <valeros08@gmail.com>
2021-03-03 12:34:08 +0200

    Add safe value for debug server when configuring debug speed

--- platform.py
+++ platform.py
@@ -184,7 +184,8 @@ class Ststm32Platform(PlatformBase):
         debug_options = copy.deepcopy(initial_debug_options)
         adapter_speed = initial_debug_options.get("speed")
         if adapter_speed:
-            server_executable = debug_options["server"].get("executable", "").lower()
+            server_options = debug_options.get("server") or {}
+            server_executable = server_options.get("executable", "").lower()
             if "openocd" in server_executable:
                 debug_options["server"]["arguments"].extend(
                     ["-c", "adapter speed %s" % adapter_speed]
