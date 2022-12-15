ef52c376521aece3eb10b19297531aa9f41ccc13
miceuz <mic@hardcore.lt>
2022-12-15 14:00:47 +0200

    Add a board level config `upload.gpiostring`
    This config can be used to specify a custom reset method during serial upload.
    
    Example:
    ```
      "upload": {
        "maximum_ram_size": 65536,
        "maximum_size": 262144,
        "protocol": "serial",
        "protocols": [
          "dfu",
          "serial",
          "jlink",
          "stlink",
          "blackmagic"
        ],
        "gpiostring": "-dtr,-rts,"
    ```
    
    Fix #257

--- builder/main.py
+++ builder/main.py
@@ -267,13 +267,13 @@ elif upload_protocol == "dfu":
 elif upload_protocol == "serial":
     def __configure_upload_port(env):
         return env.subst("$UPLOAD_PORT")
-
     env.Replace(
         __configure_upload_port=__configure_upload_port,
         UPLOADER=join(
             '"%s"' % platform.get_package_dir("tool-stm32duino") or "",
             "stm32flash", "stm32flash"),
         UPLOADERFLAGS=[
+            "-i%s" % board.get("upload.gpiostring") if board.get("upload.gpiostring", False) else "",
             "-g", board.get("upload.offset_address", "0x08000000"),
             "-b", env.subst("$UPLOAD_SPEED") or "115200", "-w"
         ],
