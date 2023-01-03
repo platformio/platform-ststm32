4e85d67383ef38ea7ca5412093ac4a0725a2e0c9
maxgerhardt <maximilian.gerhardt@rub.de>
2023-01-04 00:49:35 +0100

    Add Nucleo-F103RB LED, remove clock reset, add GPIO speed

--- examples/spl-blink/src/main.c
+++ examples/spl-blink/src/main.c
@@ -1,9 +1,16 @@
 #ifdef STM32F1
 	#include <stm32f10x_gpio.h>
 	#include <stm32f10x_rcc.h>
+	#if NUCLEO_F103RB
+	/* default on-board LED */
+	#define LEDPORT (GPIOA)
+	#define LEDPIN (GPIO_Pin_5)
+	#define ENABLE_GPIO_CLOCK (RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE))
+	#else
 	#define LEDPORT (GPIOC)
 	#define LEDPIN (GPIO_Pin_13)
 	#define ENABLE_GPIO_CLOCK (RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE))
+	#endif
 #elif STM32L1
 	#include <stm32l1xx_gpio.h>
 	#include <stm32l1xx_rcc.h>
@@ -55,12 +62,11 @@ int main(void)
 	}
 	/* gpio init struct */
 	GPIO_InitTypeDef gpio;
-	/* reset rcc */
-	RCC_DeInit();
 	/* enable clock GPIO */
 	ENABLE_GPIO_CLOCK;
 	/* use LED pin */
 	gpio.GPIO_Pin = LEDPIN;
+	gpio.GPIO_Speed = GPIO_Speed_2MHz;
 	/* set pin to push-pull output depending on the SPL variant */
 #if STM32F1
 	gpio.GPIO_Mode = GPIO_Mode_Out_PP;
