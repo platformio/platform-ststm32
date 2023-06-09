2f61825c66a15571337d9d4748fa4b5a3a9014b8
Valerii Koval <valeros08@gmail.com>
2023-06-09 17:32:39 +0300

    Drop support for legacy mbed v5

--- platform.py
+++ platform.py
@@ -52,12 +52,6 @@ class Ststm32Platform(PlatformBase):
                 self.packages["framework-cmsis"]["optional"] = False
 
         if "mbed" in frameworks:
-            deprecated_boards_file = os.path.join(
-                self.get_dir(), "misc", "mbed_deprecated_boards.json")
-            if os.path.isfile(deprecated_boards_file):
-                with open(deprecated_boards_file) as fp:
-                    if board in json.load(fp):
-                        self.packages["framework-mbed"]["version"] = "~6.51506.0"
             self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.90201.0"
 
         if "cmsis" in frameworks:
