eb8811c24891f84af0b6884a27d0f1316a1ee4f9
Bryce Happel Walton <brycewaltonmail@gmail.com>
2024-06-15 17:40:55 -0400

    Update platform.py

--- platform.py
+++ platform.py
@@ -53,7 +53,7 @@ class Ststm32Platform(PlatformBase):
                 self.packages["framework-cmsis"]["optional"] = False
 
         if "mbed" in frameworks:
-            self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.90201.0"
+            self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.90301.200702"
 
         if "cmsis" in frameworks:
             assert build_mcu, ("Missing MCU field for %s" % board)
